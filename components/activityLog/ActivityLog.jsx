54ebbf88e5dcf9fabc29331d89630e66f0936837
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2023-11-24 15:22:32 -0500

    ActivityLog page now says "someone" if the user is undefined for a photo post

--- components/activityLog/ActivityLog.jsx
+++ components/activityLog/ActivityLog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ActivityLog.css';
 import axios from 'axios';
-import {List, ListItem, Typography} from "@mui/material";
+import {Button, List, ListItem, Typography} from "@mui/material";
 
 class ActivityLog extends React.Component {
     constructor(props) {
@@ -25,8 +25,8 @@ class ActivityLog extends React.Component {
         this.fetchDataFromAPI();
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.props !== prevProps) {
+    componentDidUpdate(prevState) {
+        if (this.state !== prevState) {
             this.fetchDataFromAPI();
         }
     }
@@ -36,45 +36,47 @@ class ActivityLog extends React.Component {
         if (actions === null) {
             return <Typography>Nothing has happened recently...</Typography>
         } else {
-            return <List>
-                {actions.map((event, index) => (
-                    <ListItem key={index}>
-                        {event.type === "login" &&
-                            <Typography>
-                                {Date(event.date_time).slice(0,21)} - A user logged in!
-                            </Typography>
-                        }
-                        {event.type === "logout" &&
-                            <Typography>
-                                {Date(event.date_time).slice(0,21)} - A user logged out!
-                            </Typography>
-                        }
-                        {event.type === "register" &&
-                            <Typography>
-                                {Date(event.date_time).slice(0,21)} - A new user registered!
-                            </Typography>
-                        }
-                        {event.type === "comment" &&
-                            <>
-                                <img src={"../../images/"+event.photo_filename}
-                                     className="comment-image" alt="comment image preview"/>
+            return <>
+                <List>
+                    {actions.map((event, index) => (
+                        <ListItem key={index}>
+                            {event.type === "login" &&
                                 <Typography>
-                                    {Date(event.date_time).slice(0,21)} - {event.user} posted a new comment!
+                                    {Date(event.date_time).slice(0,21)} - A user logged in!
                                 </Typography>
-                            </>
-                        }
-                        {event.type === "photo" &&
-                            <>
-                                <img src={"../../images/"+event.photo_filename}
-                                     className="photo-image" alt="comment image preview"/>
+                            }
+                            {event.type === "logout" &&
                                 <Typography>
-                                    {Date(event.date_time).slice(0,21)} - {event.user} posted a new photo!
+                                    {Date(event.date_time).slice(0,21)} - A user logged out!
                                 </Typography>
-                            </>
-                        }
-                    </ListItem>
-                ))}
-            </List>
+                            }
+                            {event.type === "register" &&
+                                <Typography>
+                                    {Date(event.date_time).slice(0,21)} - A new user registered!
+                                </Typography>
+                            }
+                            {event.type === "comment" &&
+                                <>
+                                    <img src={"../../images/"+event.photo_filename}
+                                         className="comment-image" alt="comment image preview"/>
+                                    <Typography>
+                                        {Date(event.date_time).slice(0,21)} - {event.user} posted a new comment!
+                                    </Typography>
+                                </>
+                            }
+                            {event.type === "photo" &&
+                                <>
+                                    <img src={"../../images/"+event.photo_filename}
+                                         className="photo-image" alt="comment image preview"/>
+                                    <Typography>
+                                        {Date(event.date_time).slice(0,21)} - {event.user === null ? "Someone" : event.user} posted a new photo!
+                                    </Typography>
+                                </>
+                            }
+                        </ListItem>
+                    ))}
+                </List>
+            </>
         }
     }
 }
